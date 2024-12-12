614474e51ddbff1fe482f9925f625a910f0d7fb9
Eduardo Carlos <edu4rdobarb0za@gmail.com>
2024-12-12 01:03:38 -0300

    feat: redis cache integration

--- app/controllers/mongo/users_controller.rb
+++ app/controllers/mongo/users_controller.rb
@@ -1,14 +1,31 @@
 module Mongo
   class UsersController < ApplicationController
     def index
-      users = UserService.list_all
+      start_time = Time.current
+      users = Rails.cache.fetch("mongo_users", expires_in: 1.hour) do
+        Rails.logger.info "Cache MISS: Buscando usuários do MongoDB"
+        fetch_start = Time.current
+        result = UserService.list_all.to_a
+        Rails.logger.info "Tempo de busca no MongoDB: #{(Time.current - fetch_start).round(2)}s"
+        result
+      end
+      Rails.logger.info "Tempo total: #{(Time.current - start_time).round(2)}s"
+      Rails.logger.info "Retornando #{users.count} usuários"
       render json: users, status: :ok
     rescue StandardError => e
       render json: { error: e.message }, status: :internal_server_error
     end
 
     def show
-      user = UserService.find(params[:id])
+      start_time = Time.current
+      user = Rails.cache.fetch("mongo_user_#{params[:id]}", expires_in: 1.hour) do
+        Rails.logger.info "Cache MISS: Buscando usuário #{params[:id]} do MongoDB"
+        fetch_start = Time.current
+        result = UserService.find(params[:id])
+        Rails.logger.info "Tempo de busca no MongoDB: #{(Time.current - fetch_start).round(2)}s"
+        result
+      end
+      Rails.logger.info "Tempo total: #{(Time.current - start_time).round(2)}s"
       render json: user, status: :ok
     rescue Mongoid::Errors::DocumentNotFound
       render json: { error: 'Usuário não encontrado' }, status: :not_found
@@ -18,6 +35,7 @@ module Mongo
 
     def create
       user = UserService.create(user_params)
+      Rails.cache.delete("mongo_users")  # Invalida o cache da listagem
       render json: user, status: :created
     rescue Mongoid::Errors::Validations => e
       render json: { errors: e.document.errors.full_messages }, status: :unprocessable_entity
@@ -27,6 +45,8 @@ module Mongo
 
     def update
       user = UserService.update(params[:id], user_params)
+      Rails.cache.delete("mongo_users")  # Invalida o cache da listagem
+      Rails.cache.delete("mongo_user_#{params[:id]}")  # Invalida o cache do usuário
       render json: user, status: :ok
     rescue Mongoid::Errors::DocumentNotFound
       render json: { error: 'Usuário não encontrado' }, status: :not_found
@@ -38,6 +58,8 @@ module Mongo
 
     def destroy
       UserService.destroy(params[:id])
+      Rails.cache.delete("mongo_users")  # Invalida o cache da listagem
+      Rails.cache.delete("mongo_user_#{params[:id]}")  # Invalida o cache do usuário
       head :no_content
     rescue Mongoid::Errors::DocumentNotFound
       render json: { error: 'Usuário não encontrado' }, status: :not_found
