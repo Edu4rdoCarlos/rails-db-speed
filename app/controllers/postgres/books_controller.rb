6e44f94ae60f50cb982c16f48efa78db5528e4a3
Eduardo Carlos <edu4rdobarb0za@gmail.com>
2024-12-11 23:11:48 -0300

    update: filter by genre and title

--- app/controllers/postgres/books_controller.rb
+++ app/controllers/postgres/books_controller.rb
@@ -2,6 +2,11 @@ module Postgres
   class BooksController < Postgres::ApplicationController
     def index
       books = Postgres::Book.all
+
+      books = books.where(genre: params[:genre]) if params[:genre].present?
+
+      books = books.where('title ILIKE ?', "%#{params[:title]}%") if params[:title].present?
+
       render json: books, status: :ok
     rescue StandardError => e
       render json: { error: e.message }, status: :internal_server_error
